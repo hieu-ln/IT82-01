b206b46e9eaff6c304f336cba384c813f114aff1
kyanh2000nt <1851050014cuong@ou.edu.vn>
2019-08-05 12:51:15 +0700

    bt nhom c4

---
+++ CodeC4/QuocCuong_C4_Cau2.1_Cau2.2.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//int count = 8;
+//struct node
+//{
+//	int info;
+//	node* left;
+//	node* right;
+//};
+//node* root;
+//
+//void init()
+//{
+//	root = NULL;
+//}
