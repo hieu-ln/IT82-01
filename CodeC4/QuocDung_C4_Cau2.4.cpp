51a109abc1e86b837487542dbe6f3fc148ad7a0c
CosmicFishi <1851050025dung@ou.edu.vn>
2019-08-05 08:23:46 +0700

    Nộp chương 4

---
+++ CodeC4/QuocDung_C4_Cau2.4.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+struct node {
+	int info;
+	node *left, *right;
+};
+//Câu 2.4: tìm kiếm trong cây nhị phân tìm kiếm không dùng đệ quy
+node *search(node *p, int x)
+{
+	while (p != NULL)
+	{
+		if (p->info == x)
+			return p;
+		else
+			if (p->info < x)
+				p = p->right;
+			else
+				p = p->right;
+	}
+}
