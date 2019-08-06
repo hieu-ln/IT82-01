39f5d1a8a222ed88bcde62099cd77bee9e114fb1
ngocanhou1822 <1854050010anh@ou.edu.vn>
2019-08-06 08:38:06 +0700

    bai tap lam them

---
+++ CodeC4/NgocAnh_C4_bai2.8.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+struct node
+{
+	int info;
+	node *left, *right;
+};
+// Viet thu tuc duyet cay theo thu tu LRN (dung QUEUE)
+void LRN(TREE Root)
+{
+	if(Root != NULL)
+	{
+		LRN(Root->Left)
+			LRN(Root->Right); // xử lý Root, xử lý tương ứng theo nhu cầu
+	}
+}
